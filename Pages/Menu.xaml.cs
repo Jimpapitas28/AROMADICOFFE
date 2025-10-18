6aea6ef01dd4d860b9cd6de5acf2fcffb78218e5
Jimpapitas28 <marlendevasquezz88@gmail.com>
2025-10-17 22:42:25 -0600

    EL CODIGO DE SNACK SIRVE PARA CAFES Y POSTRES

--- Pages/Menu.xaml.cs
+++ Pages/Menu.xaml.cs
@@ -6,4 +6,9 @@ public partial class Menu : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Snacks_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Snacks());
+    }
 }
